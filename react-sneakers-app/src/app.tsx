1d27385c4a721fef52e46bbe862d8ea271cc008a
Alex <manyakinsasha@gmail.com>
2025-07-10 22:07:06 +0300

    forbid scroll page when drawer is open

--- react-sneakers-app/src/app.tsx
+++ react-sneakers-app/src/app.tsx
@@ -4,10 +4,22 @@ import HomePage from "./pages/home-page/home-page.tsx";
 import { Header } from "./shared/header/header.tsx";
 import { CartDrawer } from "./shared/drawer/cart-drawer.tsx";
 import { CartDrawerContext } from "./context/cart-drawer-context.ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  useEffect(() => {
+    if (isDrawerOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isDrawerOpen]);
 
   return (
     <CartDrawerContext.Provider value={{isDrawerOpen, setIsDrawerOpen}}>
