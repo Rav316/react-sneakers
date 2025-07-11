e34c285b06ff54cd0ffb2cc7c4d5eaa4c63dfca4
Alex <manyakinsasha@gmail.com>
2025-07-11 09:37:28 +0300

    initialize spring boot project

---
+++ react-sneakers-api/src/main/java/ru/alex/reactsneakersapi/ReactSneakersApiApplication.java
@@ -0,0 +1,13 @@
+package ru.alex.reactsneakersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSneakersApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactSneakersApiApplication.class, args);
+    }
+
+}
---
+++ react-sneakers-api/src/test/java/ru/alex/reactsneakersapi/ReactSneakersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex.reactsneakersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSneakersApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
