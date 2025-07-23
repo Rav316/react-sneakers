8c2a46bdefea93ce080970c9af9b01cee0a16aa8
Alex <manyakinsasha@gmail.com>
2025-07-23 22:59:29 +0300

    small changes in user register validation

--- react-sneakers-api/src/main/java/ru/alex/reactsneakersapi/dto/user/UserRegisterDto.java
+++ react-sneakers-api/src/main/java/ru/alex/reactsneakersapi/dto/user/UserRegisterDto.java
@@ -2,14 +2,17 @@ package ru.alex.reactsneakersapi.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record UserRegisterDto(
         @NotNull
+        @Size(min = 2)
         String name,
         @NotNull
         @Email
         String email,
         @NotNull
+        @Size(min = 4)
         String password
 ) {
 }
